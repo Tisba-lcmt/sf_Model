50ff4bcad001173d79961e9dedae3c70effe4458
David Robert <contact.davidrobert@gmail.com>
2020-11-17 10:20:31 +0100

    page article avec id en param d'url

--- src/Controller/PagesController.php
+++ src/Controller/PagesController.php
@@ -39,4 +39,38 @@ class PagesController
         // je retourne la variable $response qui contient ma Réponse HTTP
         return $response;
     }
+
+
+    /**
+     * @Route("/article", name="article_show")
+     *
+     * je passe en parametre de la méthode de controleur
+     * la classe Request associée à la variable Request
+     * pour utiliser le mécanisme d'autowire de Symfony
+     * cad : Symfony va instancier automatiquement la classe Request
+     * dans la variable $request
+     */
+    public function articleShow(Request $request)
+    {
+        // j"utilise la classe Request pour récupérer le parametre d'url id
+        $idArticle = $request->query->get('id');
+
+        // simulation d'une requête en BDD qui récupère tous les articles
+        // c'est un array (tableau) à index numérique
+        $articles = [
+            1 => 'Article 1',
+            2 => "Article 2",
+            3 => "Article 3",
+            4 => 'Article 4',
+            5 => "Article 5",
+            6 => "Article 6",
+        ];
+
+        // je créé une réponse HTTP contenant la valeur de l'article
+        // qui correspond à l'id passé en URL
+        $response = new Response('<h1>'.$articles[$idArticle].'</h1>');
+
+        // je retourne ma réponse
+        return $response;
+    }
 }
