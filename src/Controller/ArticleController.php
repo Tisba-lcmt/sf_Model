62bfddd1dcec8cffd1fe0e76b3e5ff2f58940227
David Robert <contact.davidrobert@gmail.com>
2020-11-24 13:51:57 +0100

    repository méthode find()

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -29,4 +29,26 @@ class ArticleController extends AbstractController
         // les afficher dans un fichier twig
     }
 
+
+    /**
+     * je créé une url avec une wildcard "id"
+     * qui contiendra l'id d'un article
+     * @Route("/article/{id}", name="article_show")
+     *
+     * en parametre de la méthode, je récupère la valeur de la wildcard id
+     * et je demande en plus à symfony d'instancier pour moi
+     * la classe ArticleRepository dans une variable $articleRepository
+     * (autowire)
+     */
+    public function articleShow($id, ArticleRepository $articleRepository)
+    {
+        // j'utilise l'articleRepository avec la méthode find
+        // pour faire une requête SQL SELECT en BDD
+        // et retrouver l'article dont l'id correspond à l'id passé en URL
+        $article = $articleRepository->find($id);
+
+        
+        dump($article); die;
+    }
+
 }
